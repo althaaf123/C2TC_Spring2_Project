2223a70dc459cc61f7c9bc07c56855be3db0141d
althaaf123 <syedalthaaf4@gmail.com>
2025-10-22 07:56:07 +0530

    Delete library-system/frontend/public/api.js

--- library-system/frontend/public/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-
-export const getBooks = () => API.get('/books');
-export const addBook = (book) => API.post('/books', book);
-export const updateBook = (id, book) => API.put(`/books/${id}`, book);
-export const deleteBook = (id) => API.delete(`/books/${id}`);
